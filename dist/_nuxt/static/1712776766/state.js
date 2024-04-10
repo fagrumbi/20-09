0f3d77dec7800fff3da81311b47abe75a0a9d09a
Marquis Abah <marquis.abah@shuttlers.ng>
2024-04-10 23:20:02 +0100

    updated dist

--- dist/_nuxt/static/1712776766/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712776766",layout:"default",error:a,serverRendered:true,routePath:b,config:{axios:{browserBaseURL:""},graphql:{clients:{default:{endpoint:"https:\u002F\u002Fvisionary-zpui.onrender.com\u002Fgraphql\u002Fquery"}}},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
