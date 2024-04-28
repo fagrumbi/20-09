58e6fd5d8b317d5bb414a945c1545d4ee3a516df
Marquis Abah <marquis.abah@shuttlers.ng>
2024-04-28 21:30:46 +0100

    update wallet

--- pages/user/dashboard/deposits.vue
+++ pages/user/dashboard/deposits.vue
@@ -114,12 +114,7 @@
         </table>
       </div> -->
       <div v-else-if="loading && !transactionsList?.length" class="bg-white rounded-lg shadow-md p-4 animate-pulse">
-        <!-- Header -->
-        <!-- <div class="w-2/3 h-4 bg-gray-300 rounded mb-2"></div> -->
-        <!-- Body -->
         <div class="w-full h-32 bg-gray-300 rounded mb-2"></div>
-        <!-- <div class="w-full h-8 bg-gray-300 rounded mb-2"></div>
-        <div class="w-1/2 h-8 bg-gray-300 rounded"></div> -->
       </div>
       <EmptyState title="No Deposits available" desc="Please perform a withdrawal transaction" v-else />
     </div>
