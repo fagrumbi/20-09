766258b061a541a13aadbe6f348e9761e36951ce
Marquis Abah <marquis.abah@shuttlers.ng>
2024-06-21 20:00:55 +0100

    rebuild application

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
     <FooterSection />
     <div class="fixed bottom-0 right-0 m-4 z-50">
       <a href="https://wa.me/+447465803771" target="_blank">
-        <img src="@/assets/icons/whatsapp.svg" class="text-green-500 h-16 w-16" />
+        <img src="@/assets/icons/whatsapp.svg" class="text-green-500 hover:text-green-800 h-16 w-16" />
       </a>
     </div>
   </main>
